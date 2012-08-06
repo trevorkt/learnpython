c9aeeb919211d9fb384db8c033759459a8df43d0
Trevor T <trevor@lensley.com>
2012-08-06 09:17:17 -0700

    further attempts at comprehension of file commands

--- LPTHW/L15.py
+++ LPTHW/L15.py
@@ -1,33 +1,46 @@
 # import argv from sys module
+
 from sys import argv
 
 # unpack argv to two variables
+
 script, filename = argv
 
 # open 'filename', defined above via argv,
 # and assign contents to variable 'txt'
-txt = open(filename)
+
+file_object = open(filename)
 
 # print text & variable 'filename'
+
 print "Here's your file %r: " % filename
 
 # print the output of running the read
 # command with no parameters on variable
-# 'txt' (which, remember contains the
+# 'file_object' (which, remember contains the
 # contents of 'filename')
-print txt.read()
 
-print "Type the filename again:"
+print file_object.read()
+
 # require input from user of the next
 # file to be opened, read, and printed
 # (assumedly the same file), and set
-# user input to variable 'file_again'
-file_again = raw_input("> ")
+# user input to variable 'filename_again'
 
-# set variable 'txt_again' to contents
-# of 'file_again' via open command
-txt_again = open(file_again)
+print "Type the filename again:"
+filename_again = raw_input("> ")
+
+# set variable 'file_object_again' to contents
+# of 'filename_again' via open command
+
+file_object_again = open(filename_again)
 
 # print output of running read command
-# on variable 'txt_again'
-print txt_again.read()
+# on variable 'file_object_again'
+
+print file_object_again.read()
+
+# close both files when finished
+
+file_object.close()
+file_object_again.close()
